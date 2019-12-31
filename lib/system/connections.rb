6cba3ed49b4d0996566e02abc05fdc9ab3743d4d
David M. Lary <dmlary@gmail.com>
2019-12-30 20:53:13 -0600

    restoring System::Connections.view

--- lib/system/connections.rb
+++ lib/system/connections.rb
@@ -5,6 +5,10 @@ module System::Connections
   IDLE_TIMEOUT = 5 * 60           # XXX not implemented
   DISCONNECT_TIMEOUT = 30 * 60
 
+  def self.view
+    @view ||= World.get_view(all: ConnectionComponent)
+  end
+
   def self.update(entity, conn_comp)
     conn = conn_comp.conn or return
 
