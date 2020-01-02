cb84a39fb6536895f25c2e78352b401646631ec8
David M. Lary <dmlary@gmail.com>
2020-01-01 22:37:39 -0600

    Added Vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
