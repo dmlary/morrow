e5adfc2d71a732a3ee281427b3526db64c3b23bb
David M. Lary <dmlary@gmail.com>
2020-02-15 17:11:10 -0600

    added goto

---
+++ lib/morrow/command/goto.rb
@@ -0,0 +1,20 @@
+module Morrow::Command::Goto
+  extend Morrow::Command
+
+  class << self
+    # Travel to a specific location in the world
+    #
+    # Syntax: goto <entity id>
+    #
+    def goto(actor, arg)
+      command_error 'Where would you like to go?' unless arg
+
+      dest = get_component(arg, :location)&.entity
+      dest ||= arg
+
+      move_entity(entity: actor, dest: dest, look: true)
+    rescue Morrow::EntityManager::UnknownId
+      command_error 'That does not exist in the world'
+    end
+  end
+end
